efa34ea70ed3338d21ceac61d0a737991ddc77f6
Chinseok Lee <me@pyhub.kr>
2023-06-07 21:13:47 +0900

    웹소켓 연결마다 count 값 유지하기

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,7 +2,12 @@ from channels.generic.websocket import JsonWebsocketConsumer
 
 
 class RolePlayingRoomConsumer(JsonWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.count = 0
+
     def receive_json(self, content, **kwargs):
-        print("received:", content)
+        self.count += 1
+        content["count"] = self.count
         # Echo
         self.send_json(content)
