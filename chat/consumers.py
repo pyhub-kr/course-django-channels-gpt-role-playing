1a192d03a6cb7c48c732ae23696fae4b9ca41b32
Chinseok Lee <me@pyhub.kr>
2023-06-07 23:50:59 +0900

    GPT API 호출하여, 채팅 응답받기

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 from typing import List
 
+import openai
 from channels.generic.websocket import JsonWebsocketConsumer
 from django.contrib.auth.models import AbstractUser
 
@@ -20,7 +21,14 @@ class RolePlayingRoomConsumer(JsonWebsocketConsumer):
             self.accept()
 
             self.gpt_messages = room.get_initial_messages()
-            print(self.gpt_messages)
+
+            assistant_message = self.get_query()
+            self.send_json(
+                {
+                    "type": "assistant-message",
+                    "message": assistant_message,
+                }
+            )
 
     def receive_json(self, content, **kwargs):
         # Echo
@@ -38,3 +46,25 @@ class RolePlayingRoomConsumer(JsonWebsocketConsumer):
                 pass
 
         return room
+
+    def get_query(self, command_query: str = None, user_query: str = None) -> str:
+        if command_query is not None and user_query is not None:
+            raise ValueError("command_query 인자와 user_query 인자는 동시에 사용할 수 없습니다.")
+        elif command_query is not None:
+            self.gpt_messages.append(GptMessage(role="user", content=command_query))
+        elif user_query is not None:
+            self.gpt_messages.append(GptMessage(role="user", content=user_query))
+
+        response_dict = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=self.gpt_messages,
+            temperature=1,
+        )
+        response_role = response_dict["choices"][0]["message"]["role"]
+        response_content = response_dict["choices"][0]["message"]["content"]
+
+        if command_query is None:
+            gpt_message = GptMessage(role=response_role, content=response_content)
+            self.gpt_messages.append(gpt_message)
+
+        return response_content
