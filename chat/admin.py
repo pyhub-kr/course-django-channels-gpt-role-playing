4d2b2bb56d7750737ae8cc18153ffe10c53a69ab
Chinseok Lee <me@pyhub.kr>
2023-06-07 14:45:32 +0900

    [채팅방 생성] admin web을 통한 생성 및 확인

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import RolePlayingRoom
+from .forms import RolePlayingRoomForm
 
-# Register your models here.
+
+@admin.register(RolePlayingRoom)
+class RolePlayingRoomAdmin(admin.ModelAdmin):
+    form = RolePlayingRoomForm
+
+    def save_model(self, request, obj, form, change):
+        if change is False and form.is_valid():
+            obj.user = request.user
+
+        super().save_model(request, obj, form, change)
