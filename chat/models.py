cb113ee13a32570a4662782bcabdfd362bc8b34a
Chinseok Lee <me@pyhub.kr>
2023-06-07 10:37:35 +0900

    RolePlayingRoom 모델 정의 및 마이그레이션 파일 생성

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,52 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class RolePlayingRoom(models.Model):
+    class Language(models.TextChoices):
+        ENGLISH = "en-US", "English"
+        JAPANESE = "ja-JP", "Japanese"
+        CHINESE = "zh-CN", "Chinese"
+        SPANISH = "es-ES", "Spanish"
+        FRENCH = "fr-FR", "French"
+        GERMAN = "de-DE", "German"
+        RUSSIAN = "ru-RU", "Russian"
+
+    class Level(models.TextChoices):
+        BEGINNER = 1, "초급"
+        ADVANCED = 2, "고급"
+
+    class Meta:
+        ordering = ["-pk"]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    language = models.CharField(
+        max_length=10,
+        choices=Language.choices,
+        default=Language.ENGLISH,
+        verbose_name="대화 언어",
+    )
+    level = models.SmallIntegerField(
+        choices=Level.choices, default=Level.BEGINNER, verbose_name="레벨"
+    )
+    situation = models.CharField(max_length=100, verbose_name="상황")
+    situation_en = models.CharField(
+        max_length=100,
+        blank=True,
+        verbose_name="상황 (영문)",
+        help_text="GPT 프롬프트에 직접적으로 활용됩니다. 비워두시면, situation 필드를 번역하여 자동 반영됩니다.",
+    )
+    my_role = models.CharField(max_length=100, verbose_name="내 역할")
+    my_role_en = models.CharField(
+        max_length=100,
+        blank=True,
+        verbose_name="내 역할 (영문)",
+        help_text="GPT 프롬프트에 직접적으로 활용됩니다. 비워두시면, my_role 필드를 번역하여 자동 반영됩니다.",
+    )
+    gpt_role = models.CharField(max_length=100, verbose_name="GPT 역할")
+    gpt_role_en = models.CharField(
+        max_length=100,
+        blank=True,
+        verbose_name="GPT 역할 (영문)",
+        help_text="GPT 프롬프트에 직접적으로 활용됩니다. 비워두시면, gpt_role 필드를 번역하여 자동 반영됩니다.",
+    )
