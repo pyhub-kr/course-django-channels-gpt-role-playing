406873c21c7444b6d3c24887fc572d34b2c24e28
Chinseok Lee <me@pyhub.kr>
2023-06-07 13:26:38 +0900

    [채팅방 생성] View에서 user 속성 저장하기

--- chat/forms.py
+++ chat/forms.py
@@ -5,4 +5,13 @@ from .models import RolePlayingRoom
 class RolePlayingRoomForm(forms.ModelForm):
     class Meta:
         model = RolePlayingRoom
-        fields = "__all__"
+        fields = [
+            "language",
+            "level",
+            "situation",
+            "situation_en",
+            "my_role",
+            "my_role_en",
+            "gpt_role",
+            "gpt_role_en",
+        ]
--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,20 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView
 from .models import RolePlayingRoom
 from .forms import RolePlayingRoomForm
 
 
+@method_decorator(staff_member_required, name="dispatch")
 class RolePlayingRoomCreateView(CreateView):
     model = RolePlayingRoom
     form_class = RolePlayingRoomForm
+
+    def form_valid(self, form):
+        role_playing_room = form.save(commit=False)
+        role_playing_room.user = self.request.user
+        return super().form_valid(form)
 
 
 role_playing_room_new = RolePlayingRoomCreateView.as_view()
