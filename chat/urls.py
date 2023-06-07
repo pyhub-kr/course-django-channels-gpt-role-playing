10365a6f95153157815d42684b9a8e0bcc63b3b9
Chinseok Lee <me@pyhub.kr>
2023-06-07 16:21:18 +0900

    채팅방 삭제 페이지 구현

--- chat/urls.py
+++ chat/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path("new/", views.role_playing_room_new, name="role_playing_room_new"),
     path("<int:pk>/edit/", views.role_playing_room_edit, name="role_playing_room_edit"),
     path("<int:pk>/", views.role_playing_room_detail, name="role_playing_room_detail"),
+    path(
+        "<int:pk>/delete/",
+        views.role_playing_room_delete,
+        name="role_playing_room_delete",
+    ),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,15 @@
+from django.contrib import messages
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, UpdateView, ListView, DetailView
+from django.views.generic import (
+    CreateView,
+    UpdateView,
+    ListView,
+    DetailView,
+    DeleteView,
+)
 from .models import RolePlayingRoom
 from .forms import RolePlayingRoomForm
 
@@ -58,3 +66,22 @@ class RolePlayingRoomUpdateView(UpdateView):
 
 
 role_playing_room_edit = RolePlayingRoomUpdateView.as_view()
+
+
+@method_decorator(staff_member_required, name="dispatch")
+class RolePlayingRoomDeleteView(DeleteView):
+    model = RolePlayingRoom
+    success_url = reverse_lazy("role_playing_room_list")
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(user=self.request.user)
+        return qs
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, "채팅방을 삭제했습니다.")
+        return response
+
+
+role_playing_room_delete = RolePlayingRoomDeleteView.as_view()
