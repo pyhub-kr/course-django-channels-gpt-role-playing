b8d7f5a76003fd3fbef3a4a064eddf2792a78a9a
Chinseok Lee <me@pyhub.kr>
2023-06-07 15:28:04 +0900

    채팅방 목록 페이지 구현

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.role_playing_room_list, name="role_playing_room_list"),
     path("new/", views.role_playing_room_new, name="role_playing_room_new"),
     path("<int:pk>/edit/", views.role_playing_room_edit, name="role_playing_room_edit"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,22 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, ListView
 from .models import RolePlayingRoom
 from .forms import RolePlayingRoomForm
+
+
+@method_decorator(staff_member_required, name="dispatch")
+class RolePlayingRoomListView(ListView):
+    model = RolePlayingRoom
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(user=self.request.user)
+        return qs
+
+
+role_playing_room_list = RolePlayingRoomListView.as_view()
 
 
 @method_decorator(staff_member_required, name="dispatch")
